e6df5e4c3ef07384e728b9a7112d0bcbb00bf7db
Senth <senth.wallace@gmail.com>
2021-01-26 11:50:17 +0100

    ✨ Empty AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
 import { Route } from 'react-router'
-import ReportPage from './pages/reports'
-import SettingsPage from './pages/settings'
+import ReportPage from './pages/Reports'
+import SettingsPage from './pages/Settings'
+import AppBar from './components/AppBar'
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
+      <AppBar />
       <Route exact={true} path="/" component={ReportPage} />
       <Route path="/settings" component={SettingsPage} />
     </BrowserRouter>
---
+++ src/components/AppBar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+const index: React.FC<Props> = (props) => {
+  return <div className="appBar"></div>
+}
+
+export default index
