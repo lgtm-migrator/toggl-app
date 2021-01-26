cc66423b6cb0090ccd7b62f7ce5f38fdf051dd5a
Senth <senth.wallace@gmail.com>
2021-01-26 11:45:12 +0100

    ✨ Barebone app with settings and report page

---
+++ src/pages/reports/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+const index: React.FC<Props> = (props) => {
+  return <h1>Reports</h1>
+}
+
+export default index
---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+const index: React.FC<Props> = (props) => {
+  return (
+    <>
+      <h1>Settings</h1>
+    </>
+  )
+}
+
+export default index
