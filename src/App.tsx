322393b2239938d4fb258da9d955bada501e4174
Senth <senth.wallace@gmail.com>
2021-01-26 11:56:22 +0100

    🚚 Rename components to common

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { Route } from 'react-router'
 import ReportPage from './pages/Reports'
 import SettingsPage from './pages/Settings'
-import AppBar from './components/AppBar'
+import AppBar from './common/AppBar'
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
